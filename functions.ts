09795cb3588bf4437740580c0d9277a054d2161d
Pavan S Patil <psp3075@gmail.com>
2022-10-27 16:22:55 +0530

    functions

---
+++ functions.ts
@@ -0,0 +1,13 @@
+function addTen1(num){
+    return num + 10
+}
+
+function addTen(num:number){
+    return num + 10
+}
+
+
+
+function signUpUser(name:string,email:string,password:string,isPremium:true){
+    return 'signed up successfully'
+}

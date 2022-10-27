7418abfa45e8fd487e95c624cc3b133219ddf8ee
Pavan S Patil <psp3075@gmail.com>
2022-10-27 16:39:25 +0530

    functions

--- functions.ts
+++ functions.ts
@@ -6,6 +6,11 @@ function addTen(num:number){
     return num + 10
 }
 
+function addTen(num:number):number{
+    //return 'hello' //wrong way
+    return num+10
+}
+
 function signUpUser(name:string,email:string,password:string,isPremium:boolean=false){
     return 'signed up successfully'
 }

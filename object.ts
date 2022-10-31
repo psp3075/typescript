b3a639dde8b15017f307b8cf6a8cd9275c74f81c
Pavan S Patil <psp3075@gmail.com>
2022-10-31 13:34:43 +0530

    objects

---
+++ object.ts
@@ -0,0 +1,12 @@
+function createUser({name:string,isPaid:boolean}){}
+
+let newUser = {name:"psp",isPaid:false,email:"psp@psp.com"}
+
+createUser(newUser)
+
+createUser({name:"psp",isPaid:false,email:"psp@psp.com"}) //error
+
+
+function createCourse():{name:string,price:number}{ //return type defined :
+    return {name:"reactjs",price:999}
+}
